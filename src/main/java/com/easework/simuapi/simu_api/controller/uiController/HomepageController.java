0c8734b19651a15c296e17bea182ef4ce7a5bee9
rituraj6 <rajvermaritu15@gmail.com>
2025-02-24 16:27:34 +0530

    refactor: update HomepageController to use @Controller and adjust HTML templates for improved styling

--- src/main/java/com/easework/simuapi/simu_api/controller/uiController/HomepageController.java
+++ src/main/java/com/easework/simuapi/simu_api/controller/uiController/HomepageController.java
@@ -1,12 +1,13 @@
 package com.easework.simuapi.simu_api.controller.uiController;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
 
-@RestController
+@Controller
 public class HomepageController {
 
     @GetMapping("/")
